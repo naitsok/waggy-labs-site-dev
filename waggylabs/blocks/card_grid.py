59d28a9a945b2cafe2b5c72303fb9a804690fb33
Konstantin <contact@ktamarov.com>
2022-11-29 23:24:54 +0200

    Working on card grid block.

--- waggylabs/blocks/card_grid.py
+++ waggylabs/blocks/card_grid.py
@@ -1,8 +1,10 @@
+from django.conf import settings
 from django.utils.translation import gettext_lazy as _
 
 from wagtail.core.blocks import (
     StructBlock, CharBlock, ListBlock,
-    PageChooserBlock, StreamBlock, URLBlock
+    PageChooserBlock, StreamBlock, URLBlock,
+    ChoiceBlock
     )
 from wagtail.images.blocks import ImageChooserBlock
 
@@ -10,13 +12,45 @@ from .mathjax_markdown import MathJaxMarkdownBlock
 
 class LinksBlock(StreamBlock):
     """Block to add external links and links to Wagtail pages to a card."""
-    external_link = URLBlock(
-        required=True,
-        label=_('Link to external site'),
-        )
-    internal_link = PageChooserBlock(
-        label=_('Link to this site page'),
-    )
+    external_link = StructBlock([
+        ('link', URLBlock(
+            required=True,
+            label=_('Link to external site'),
+        )),
+        ('text', CharBlock(
+            required=True,
+            label=_('Text of the link'),
+        )),
+        ('style', ChoiceBlock(
+            choices=[
+                ('btn btn-primary', _('Button')),
+                ('card-link', _('Link')),
+            ],
+            default='card-link',
+            label=_('Style of link'),
+        )),
+    ])
+    internal_link = StructBlock([
+        ('link', PageChooserBlock(
+            label=_('Link to this site page'),
+        )),
+        ('text', CharBlock(
+            required=True,
+            label=_('Text of the link'),
+        )),
+        ('style', ChoiceBlock(
+            choices=[
+                ('btn btn-primary', _('Button')),
+                ('card-link', _('Link')),
+            ],
+            default='card-link',
+            label=_('Style of link'),
+        )),
+    ])
+    social_link = StructBlock([
+        
+    ])
+    
     
     class Meta:
         icon = 'link'
@@ -33,3 +67,48 @@ class CardBlock(StructBlock):
     class Meta:
         icon = 'form'
         label = _('Item of the card grid')
+        template = 'waggylabs/blocks/card.html'
+        
+
+DEFAULT_CARD_GRID_COLUMNS = 4
+DEFAULT_CARD_GRID_COLUMN = 3
+class CardGridBlock(StructBlock):
+    """Card grid block for StreamField."""
+    equal_height = ChoiceBlock(
+        choices=[
+            ('equal', _('Equal')),
+            ('not_equal', _('Wrap content')),
+        ],
+        default='equal',
+        label=_('Height'),
+    )
+    style = ChoiceBlock(
+        choices=[
+            ('separate', _('Separate')),
+            ('grouped', _('Grouped')),
+        ],
+        default='separate',
+        label=_('Grouping'),
+    )
+    orientation = ChoiceBlock(
+        choices=[
+            ('vertical', _('Vertical')),
+            ('horizontal', _('Horizontal')),
+        ],
+        default='vertical',
+        label=_('Orientation'),
+    )
+    columns = ChoiceBlock(
+        choices=[
+            (i + 1, i + 1) for i in 
+            range((settings.WAGGYLABS_CARD_GRID_COLUMNS if 
+                   hasattr(settings, 'WAGGYLABS_CARD_GRID_COLUMNS') 
+                   else DEFAULT_CARD_GRID_COLUMNS))
+        ],
+        default=(settings.WAGGYLABS_CARD_GRID_COLUMN if 
+                 hasattr(settings, 'WAGGYLABS_CARD_GRID_COLUMN') 
+                 else DEFAULT_CARD_GRID_COLUMN),
+        label=_('Number of columns'),
+    )
+    cards = ListBlock(CardBlock())
+    
