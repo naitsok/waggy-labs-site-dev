99152f27614680e869ff7a28d70797665d07395b
Konstantin Tamarov <contact@ktamarov.com>
2023-03-06 16:44:28 +0200

    Working on post categories.

--- waggylabs/blocks/post_categories.py
+++ waggylabs/blocks/post_categories.py
@@ -1,7 +1,7 @@
 from django.utils.translation import gettext_lazy as _
 
 from wagtail.blocks import (
-    ChoiceBlock, StructBlock, CharBlock
+    ChoiceBlock, StructBlock, CharBlock, PageChooserBlock
 )
 
 from waggylabs.blocks.icon import IconBlock, IconLocationBlock
@@ -13,6 +13,16 @@ from waggylabs.widgets import DisabledOptionSelect
 
 class PostCategoriesBlock(StructBlock):
     """Block to show post categories."""
+    post_list_page = PageChooserBlock(
+        required=False,
+        page_type='waggylabs.models.post_list_page.PostListPage',
+        label=_('Root post list page'),
+        help_text=_('Shows post categories for the posts, which are '
+                    'children of the selected post list page. '
+                    'If left empty, the currently browsed post list page will '
+                    'be used, or all categories will be displayed if the '
+                    ' currently browsed page is not a post lit page.')
+    )
     style = CardStyleChoiceBlock(
         required=False,
         label=_('Style')
